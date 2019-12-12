2fbe1f56e3fe9e04377a030daff1a3d3b3297e77
rmparr <ryan@ryanmparr.com>
2019-12-12 17:31:26 +0100

    Update puma config to run on Uberspace. Set the APP_ROOT
    Reduce the number of threads
    
    Remove daemonize

--- config/puma.rb
+++ config/puma.rb
@@ -3,19 +3,18 @@
 if ENV['RAILS_ENV'] == 'production'
 
   # First, you need to change these below to your situation.
-  APP_ROOT = '/home/discourse/discourse'
+  APP_ROOT = '/home/grngdgtl/discourse/current'
   num_workers = ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 4
 
   # Second, you can choose how many threads that you are going to run at same time.
   workers "#{num_workers}"
-  threads 8, 32
+  threads 2, 4
 
   # Unless you know what you are changing, do not change them.
   bind "unix://#{APP_ROOT}/tmp/sockets/puma.sock"
   stdout_redirect "#{APP_ROOT}/log/puma.log", "#{APP_ROOT}/log/puma.err.log"
   pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
   state_path "#{APP_ROOT}/tmp/pids/puma.state"
-  daemonize true
   preload_app!
 
 end
